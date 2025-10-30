233839ed96398aa66396ffda184816a236b6590c
santiago <paiva.santiago@gmail.com>
2025-10-30 08:25:55 -0400

    add --debug flag

--- scripts/subscribe.sh
+++ scripts/subscribe.sh
@@ -3,3 +3,6 @@
 
 PROXY_IP="your-ip-here"
 mump2p --disable-auth --client-id="hackathon" subscribe --topic=mytopic --service-url="http://${PROXY_IP}:8080"
+
+# Use the following command to turn on debugging mode for analysis
+#mump2p --debug --disable-auth --client-id="hackathon" subscribe --topic=mytopic --service-url="http://${PROXY_IP}:8080"
